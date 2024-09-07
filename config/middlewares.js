face89746ad5a1c6e4496b26d7ff8f81e79bc699
Hayreal <1135564544@qq.com>
2024-09-08 01:22:48 +0800

    cors updated to  *

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,16 +2,8 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  // 'strapi::cors',
+  'strapi::cors',
   'strapi::poweredBy',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: false,
-      header: '*',
-      origin: '*'
-    }
-  },
   'strapi::query',
   'strapi::body',
   'strapi::session',
